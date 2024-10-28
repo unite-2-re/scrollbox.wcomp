c90ed52a11ff70eb6f46529ee11825fcb7ee7d5d
unit-404 <conway24b@gmail.com>
2024-10-28 20:14:22 +0700

    fix zoom classlist conflict

--- src/$core$/Zoom.ts
+++ src/$core$/Zoom.ts
@@ -10,7 +10,9 @@ export const getZoom = ()=>{
 let zoomValue = getZoom() || 1;
 document.documentElement.addEventListener("scaling", ()=>{ zoomValue = getZoom() || 1; });
 document.documentElement.addEventListener("resize", ()=>{ zoomValue = getZoom() || 1; });
-document.documentElement.classList.add("__exp-use-zoom");
+if (!document.documentElement.classList.contains("__exp-use-zoom")) {
+    document.documentElement.classList.add("__exp-use-zoom");
+}
 addEventListener("resize", ()=>{ zoomValue = getZoom() || 1; });
 
 //
